86eda5c03f45dba972857786e89b5e6b06c9b24e
mialana <96770662+mialana@users.noreply.github.com>
2025-08-03 23:00:28 -0400

    Finalize Robert's Cross Outline detection

--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -27,7 +27,7 @@ float RobertsCross(float3 samples[4])
 {
     const float3 difference_1 = samples[1] - samples[2];
     const float3 difference_2 = samples[0] - samples[3];
-    return sqrt(dot(difference_1, difference_1) + dot(difference_2, difference_2));
+    return sqrt(dot(difference_1, difference_1) + dot(difference_2, difference_2)) * _Robert_s_Cross_Multiplier;
 }
 
 // The same kernel logic as above, but for a single-value instead of a vector3.
@@ -35,10 +35,10 @@ float RobertsCross(float samples[4])
 {
     const float difference_1 = samples[1] - samples[2];
     const float difference_2 = samples[0] - samples[3];
-    return sqrt(difference_1 * difference_1 + difference_2 * difference_2);
+    return sqrt(difference_1 * difference_1 + difference_2 * difference_2) * _Robert_s_Cross_Multiplier;
 }
 
-void ComputeOutlines_float(float2 uv, out float4 Color)
+void ComputeOutlines_float(float2 uv, out float4 OUT)
 {
     float2 texel_size = float2(1.0 / _ScreenParams.x, 1.0 / _ScreenParams.y);
 
@@ -68,19 +68,37 @@ void ComputeOutlines_float(float2 uv, out float4 Color)
     float edge_luminance = RobertsCross(luminance_samples);
 
     // Threshold the edges (discontinuity must be above certain threshold to be counted as an edge). The sensitivities are hardcoded here.
-    float depth_threshold = 1 / 200.0f;
+
+    float thisDepth;
+    GetDepth_float(uv, thisDepth);
+    float depth_threshold = thisDepth * _Depth_Threshold;
     edge_depth = edge_depth > depth_threshold ? 1 : 0;
 
-    float normal_threshold = 1 / 4.0f;
-    edge_normal = edge_normal > normal_threshold ? 1 : 0;
+    edge_normal = edge_normal > _Normal_Threshold ? 1 : 0;
 
-    float luminance_threshold = 1 / 0.5f;
-    edge_luminance = edge_luminance > luminance_threshold ? 1 : 0;
+    edge_luminance = edge_luminance > _Luminance_Threshold ? 1 : 0;
 
     // Combine the edges from depth/normals/luminance using the max operator.
     float edge = max(edge_depth, max(edge_normal, edge_luminance));
 
+    // float edge = edge_luminance;
+
     // Color the edge with a custom sceneColor.
-    GetSceneColor_float(uv, Color);
-    Color = Color + edge * _Outline_Color;
+    float4 sceneColor;
+    GetSceneColor_float(uv, sceneColor);
+    if (_Debug_View) {
+        if (edge == 0) {
+            OUT = sceneColor;
+        }
+        else if (edge == edge_depth) {
+            OUT = float4(1, 0, 0, 1);
+        } else if (edge == edge_normal) {
+            OUT = float4(0, 1, 0, 1);
+        } else {
+            OUT = float4(0, 0, 1, 1);
+        }
+    } 
+    else {
+        OUT = edge == 1 ? _Outline_Color : sceneColor;
+    }
 }
