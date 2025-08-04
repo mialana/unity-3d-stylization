655cb193bf25b2f586504b0d33d96ec7e6c4e7b9
mialana <96770662+mialana@users.noreply.github.com>
2025-08-04 13:25:11 -0400

    Improve efficiency

--- Assets/Render Settings/Render Features/Full Screen Feature.cs
+++ Assets/Render Settings/Render Features/Full Screen Feature.cs
@@ -81,8 +81,8 @@ public class FullScreenFeature : ScriptableRendererFeature
     // This method is called when setting up the renderer once per-camera.
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        // if (renderingData.cameraData.cameraType != CameraType.Game)
-        //     return;
+        if (renderingData.cameraData.cameraType != CameraType.Game && renderingData.cameraData.cameraType != CameraType.SceneView)
+            return;
         renderer.EnqueuePass(m_FullScreenPass);
     }
 }
