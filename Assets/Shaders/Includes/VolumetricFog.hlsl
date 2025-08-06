ea687f1cc439d472738ddd4d77ce84d3f5d15ca9
mialana <96770662+mialana@users.noreply.github.com>
2025-08-05 22:06:18 -0400

    Add animation and second layer to volumetric fog

--- Assets/Shaders/Includes/VolumetricFog.hlsl
+++ Assets/Shaders/Includes/VolumetricFog.hlsl
@@ -3,19 +3,32 @@ SAMPLER(sampler_TrilinearRepeat);
 
 float henyey_greenstein(float angle, float scattering)
 {
-    return (1.0 - angle * angle) / (4.0 * PI * pow(1.0 + scattering * scattering - (2.0 * scattering) * angle, 1.5f));
+    return (1.0 - angle * angle) / (4.0 * PI * pow(abs(1.0 + scattering * scattering - (2.0 * scattering) * angle), 1.5f));
 }
 
 float get_density(float3 worldPos)
 {
-    float4 noise = _Fog_3D_Noise_Tex.SampleLevel(sampler_TrilinearRepeat, worldPos * 0.01 * _3D_Noise_Tiling, 0);
-    float density = dot(noise, noise);
+    float delta = _Time.y;
+    float startTime = delta;
+
+    float3 fogMovement = -_Fog_Direction * delta * _Fog_Speed;
+    // Two noise layers, with slightly different scale and motion
+    float3 uv1 = worldPos * 0.01 * _3D_Noise_Tiling + fogMovement;
+    float3 uv2 = worldPos * 0.025 * _3D_Noise_Tiling + fogMovement * 1.3;
+
+    // Sample raw noise values
+    float base = _Fog_3D_Noise_Tex.SampleLevel(sampler_TrilinearRepeat, uv1, 0).r;
+    float detail = _Fog_3D_Noise_Tex_2.SampleLevel(sampler_TrilinearRepeat, uv2, 0).r;
+
+    // Weighted additive blend
+    float density = base + detail * 0.4;  // tweak 0.4 to control detail's influence
     density = saturate(density - _Density_Threshold) * _Density_Multiplier;
+
 
     return density;
 }
 
-void ComputeFog_float(float2 uv, out float4 OutColor)
+void ComputeFog_float(float2 uv, float Time, out float4 OutColor)
 {
     float4 sceneColor = SAMPLE_TEXTURE2D(_MainTex, sampler_point_clamp, uv);
     float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(uv);
@@ -31,7 +44,7 @@ void ComputeFog_float(float2 uv, out float4 OutColor)
     float distTravelled = InterleavedGradientNoise(pixelCoords, (int)(_Time.y / max(HALF_EPS, unity_DeltaTime.x))) * _Noise_Offset;
     float transmittance = 1;
     float4 fogCol = _Fog_Color;
-
+#ifndef SHADERGRAPH_PREVIEW
     while (distTravelled < distLimit)
     {
         float3 rayPos = entryPoint + rayDir * distTravelled;
@@ -39,16 +52,18 @@ void ComputeFog_float(float2 uv, out float4 OutColor)
         if (density > 0)
         {
             Light mainLight = GetMainLight(TransformWorldToShadowCoord(rayPos));
-            fogCol.rgb += mainLight.color.rgb * _Light_Contribution_Color.rgb * henyey_greenstein(dot(rayDir, mainLight.direction), _Light_Scattering) * density * mainLight.shadowAttenuation * _Step_Size;
+
             if (mainLight.shadowAttenuation) {
-                fogCol.rgb += _Light_Contribution_Offset;
+                density *= _Light_Contribution_Scalar;
             }
+
+            fogCol.rgb += mainLight.color.rgb * _Light_Contribution_Color.rgb * henyey_greenstein(dot(rayDir, mainLight.direction), _Light_Scattering) * density * mainLight.shadowAttenuation * _Step_Size;
             transmittance *= exp(-density * _Step_Size);
         }
         distTravelled += _Step_Size;
     }
-
+#endif
     OutColor = lerp(sceneColor, fogCol, 1.f - saturate(transmittance));
 
-    Light mainLight = GetMainLight(TransformWorldToShadowCoord(worldPos));
+    // Light mainLight = GetMainLight(TransformWorldToShadowCoord(worldPos));
 }
